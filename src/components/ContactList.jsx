d455eac0788f63acc4429cd0ac86e3558f2e2f6d
Robert Yohnka <robertyohnka@Roberts-MacBook-Pro.local>
2024-04-10 13:33:30 -0400

    thruFetchingTab

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -10,7 +11,19 @@ const dummyContacts = [
 
 export default function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts)
-    // console.log("Contacts: ", contacts)
+    useEffect(() => {
+        async function fetchContacts() {
+            try {
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json();
+                setContacts(result);
+            } catch (error) {
+              console.error(error);  
+            }
+        }
+        fetchContacts()
+    }, [])
+    console.log("Contacts in state: ", contacts);
     return (
         <table>
             <thead>
